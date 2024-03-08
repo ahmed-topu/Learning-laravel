c4471e48b9990ff124667e4289ffcd7302895485
ahmed-topu <stopu.info@gmail.com>
2024-03-08 23:14:16 +0600

    service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+// use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +13,17 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        // app()->bind('first_service_provider', function ($app) {
+        //     dd("This is my first service provider");
+        // });
+
+        // App::getFacadeApplication()->bind('second_service_provider', function ($app) {
+        //     dd("This is my second service provider");
+        // });
+
+        // app()->bind('third_service_provider', function ($app) {
+        //     dd("This is my third service provider");
+        // });
     }
 
     /**
